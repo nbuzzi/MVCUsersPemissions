04d6fce2eaa959a125d67a5741160eafd4410b54
Nicolás Buzzi <nico.cbuzzi@gmail.com>
2018-06-26 23:50:36 -0300

    Initial commit

---
+++ MVCRouting/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿namespace MVCRouting.Controllers
+{
+    using System.Linq;
+    using System.Web.Mvc;
+    using MVCRouting.Models;
+
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            if (Session["User"] is UserViewModel user
+                && user.Views.FirstOrDefault(m => m.Equals("About")) != null)
+            {
+                return View();
+            }
+
+            return View("~/Views/Home/Error.cshtml");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserViewModel userViewModel)
+        {
+            if(userViewModel.Name.Equals("admin") && userViewModel.Password.Equals("1234"))
+            {
+                userViewModel.Views.Add("About");
+                Session["User"] = userViewModel;
+
+                return RedirectToAction("About");
+            }
+
+            return Json("Wrong user/password");
+        }
+    }
+}
---
+++ MVCRouting/Models/UserViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace MVCRouting.Models
+{
+    using System.Collections.Generic;
+
+    public class UserViewModel
+    {
+        public UserViewModel()
+        {
+            Views = new List<string>();
+        }
+
+        public string Name { get; set; }
+        public string Password { get; set; }
+
+        public List<string> Views { get; set; }
+    }
+}
